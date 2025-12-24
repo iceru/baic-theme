fca97d7603df4653b5704166d868205f55c3367f
Muhamad Hafiz <m.hafiz1825@gmail.com>
2025-12-24 11:22:12 +0700

    feat: update homepage

--- page-homepage.php
+++ page-homepage.php
@@ -6,7 +6,7 @@
         $banners = new WP_Query(['post_type' => 'banner', 'posts_per_page' => 5]);
         while ($banners->have_posts()):
             $banners->the_post(); ?>
-            <div class="relative h-[96vh] w-full">
+            <div class="relative h-[96vh] w-full aspect-video">
                 <?php the_post_thumbnail('full', ['class' => 'w-full h-full object-cover']); ?>
                 <div class="absolute inset-0 flex items-center px-12">
                     <h2 class="text-white text-6xl font-bold uppercase"><?php the_title(); ?></h2>
@@ -60,12 +60,112 @@
     </div>
 </section>
 
-<section id="promotions">
+<section class="bg-jhl-gray-1 text-white relative flex justify-between">
 
+    <div class="container py-14">
+        <h3 class="max-w-[349px] text-4xl leading-[39px] mb-4">
+            BUAT JANJI TEST DRIVE ANDA
+        </h3>
+        <a href=""
+            class="border border-white rounded-full px-7 py-[18.5px] inline-flex items-center space-x-4 text-xs font-semibold tracking-wider">
+            <span>Hubungi Kami</span>
+            <img src="<?php echo get_template_directory_uri() ?>/images/arrow-white.png" alt="">
+        </a>
+    </div>
+    <div class="right-0 absolute h-full">
+        <img src="<?php echo get_template_directory_uri() ?>/images/dashboard-car.png"
+            class="h-full w-auto object-contain" alt="">
+    </div>
 </section>
 
-<section id="dealers">
 
+
+<section class="py-20 bg-beijing-black" id="promotions">
+    <div class="container">
+        <h2 class="text-[28px] md:text-[44px] mb-8 text-white">
+            PROMOTIONS
+        </h2>
+        <div class="grid md:grid-cols-5 gap-6">
+            <?php
+            $promo_query = new WP_Query([
+                'post_type' => 'promotion', // Change to your CPT slug
+                'posts_per_page' => 5,
+            ]);
+
+            if ($promo_query->have_posts()):
+                while ($promo_query->have_posts()):
+                    $promo_query->the_post(); ?>
+                    <div>
+                        <div class="mb-8">
+                            <?php if (has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail('large', ['class' => 'rounded-lg w-full h-auto']); ?>
+                            <?php else: ?>
+                                <img src="<?php echo get_template_directory_uri() ?>/images/promo-1.png" alt="" class="rounded-lg">
+                            <?php endif; ?>
+                        </div>
+                        <h5 class="leading-[22px] font-medium mb-8 line-clamp-2 !text-white">
+                            <?php the_title(); ?>
+                        </h5>
+                        <a href="<?php the_permalink(); ?>"
+                            class="text-xs text-jhl-gray-1 font-semibold uppercase tracking-wide inline-flex space-x-[10px] items-center">
+                            <div>
+                                <img src="<?php echo get_template_directory_uri() ?>/images/chev-right.png" alt="">
+                            </div>
+                            <span>Learn More</span>
+                        </a>
+                    </div>
+                <?php endwhile;
+                wp_reset_postdata();
+            endif; ?>
+        </div>
+    </div>
+</section>
+
+<section class="py-[100px] text-jhl-gray-1" id="dealers">
+    <div class="container">
+        <div class="text-center mb-10 flex justify-between items-center">
+            <h2 class="text-[28px] font-light">
+                FIND A DEALER
+            </h2>
+            <a href="<?php echo get_post_type_archive_link('dealer'); ?>"
+                class="inline-flex items-center space-x-[10px] text-sm font-semibold text-jhl-gray-2 ">
+                <div>
+                    <img src="<?php echo get_template_directory_uri() ?>/images/chev-right.png" alt="">
+                </div>
+                <span>TELUSURI</span>
+            </a>
+        </div>
+        <div class="grid md:grid-cols-4 gap-4">
+            <?php
+            $dealer_query = new WP_Query([
+                'post_type' => 'dealer', // Change to your CPT slug
+                'posts_per_page' => 4,
+            ]);
+
+            if ($dealer_query->have_posts()):
+                while ($dealer_query->have_posts()):
+                    $dealer_query->the_post(); ?>
+                    <div>
+                        <div class="mb-4">
+                            <?php if (has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail('large', ['class' => 'rounded-lg h-[318px] object-cover w-full']); ?>
+                            <?php else: ?>
+                                <img src="<?php echo get_template_directory_uri() ?>/images/alsut.png" alt=""
+                                    class="rounded-lg h-[318px] object-cover w-full">
+                            <?php endif; ?>
+                        </div>
+                        <h4 class="leading-7 text-xl mb-4 text-jhl-black">
+                            <?php the_title(); ?>
+                        </h4>
+                        <div class="body">
+                            <?php echo wp_trim_words(get_the_excerpt(), 20); ?>
+                        </div>
+                    </div>
+                <?php endwhile;
+                wp_reset_postdata();
+            endif; ?>
+        </div>
+    </div>
 </section>
 
 <section id="socials">
